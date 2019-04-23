ec7cae383f6557bbaee4edf133f619a012332a27
SanderEY <49651047+SanderEY@users.noreply.github.com>
2019-04-23 10:23:31 +0200

    Add files via upload

--- IRB models/model.py
+++ IRB models/model.py
@@ -55,3 +55,25 @@ class model(object):
           monitoring [var_name] = pd.DataFrame(list(itertools.islice(y2, len(monitoring .iloc[:,1])))).values
           
           return development, monitoring
+      
+     def CCF_model(self, FEATURES, LABEL, development, monitoring, var_name):
+     
+          def input_fn(data_set):
+              feature_cols = {k: tf.constant(data_set[k].values) for k in FEATURES} 
+              labels = tf.constant(data_set[LABEL].values)
+              return feature_cols, labels
+              
+          feature_cols = [tf.contrib.layers.real_valued_column(k) for k in FEATURES]
+          
+          regressor = tf.contrib.learn.DNNRegressor(
+            feature_columns=feature_cols, hidden_units=[64, 32, 16], optimizer=tf.train.ProximalAdagradOptimizer(
+                learning_rate=0.1,
+                l1_regularization_strength=0.001) )
+          regressor.fit(input_fn=lambda: input_fn(development), steps=56)
+          
+          y1 = regressor.predict(input_fn=lambda: input_fn(development))
+          y2 = regressor.predict(input_fn=lambda: input_fn(monitoring))
+          development[var_name] = pd.DataFrame(list(itertools.islice(y1, len(development.iloc[:,1])))).values
+          monitoring [var_name] = pd.DataFrame(list(itertools.islice(y2, len(monitoring .iloc[:,1])))).values
+          
+          return development, monitoring
