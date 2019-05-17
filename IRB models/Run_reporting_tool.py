8229b5e55a1755e8c6f7ff4ac420203eb0f1abc2
Brent Oeyen <brent.oeyen@be.ey.com>
2019-05-17 17:35:24 +0200

    Added HI exposure weighted calculation

--- IRB models/Run_reporting_tool.py
+++ IRB models/Run_reporting_tool.py
@@ -22,7 +22,7 @@ from Validation_tests import *
 ####### Occurrence of overrides (2.5.2.2)
 ####### Occurence of technical defaults (2.5.2.3)
 ####### Predictve ability (2.5.3)
-jeffrey_test = PD_tests().Jeffrey(development_set)
+jeffrey_test = PD_tests().Jeffrey(development_set[['grade', 'PD', 'Default_Binary']], 'grade', 'PD', 'Default_Binary')
 ####### Discriminatory power test - AUC (2.5.4)
 PD_AUC_val, PD_s_val = PD_tests().AUC(validation_set.Default_Binary, validation_set.grade_num, 1)
 PD_AUC_dev, PD_s_dev = PD_tests().AUC(development_set.Default_Binary, development_set.grade_num, 1)
@@ -36,30 +36,22 @@ AUC_bootstrap = extra_tests().boot( development_set, 'out.append(PD_tests().AUC(
 transition_matrix        = matrix().matrix_obs(development_set, 'grade_num', 'Bin_PD', 'Default_Binary')
 transition_matrix_freq   = matrix().matrix_prob(transition_matrix)
 ### Customer migrations (2.5.5.1)
-# To be developped
 # Create YYYY_rating column with a rating for each facility for each year
-
 upper_MWB, lower_MWB = PD_tests().MWB(transition_matrix, transition_matrix_freq)
-
 ### Stability of migration matrix (2.5.5.2)
 z_up, z_low, zUP_pval, zDOWN_pval = PD_tests().stability_migration_matrix(transition_matrix, transition_matrix_freq)
-
 ### Concentration in rating grades (2.5.5.3)
 # calculate coefficient of variation and the herfindahl index
-CV_init, HI_init, CV_curr, HI_curr, cr_pval = PD_tests().Herfindahl(development_set, validation_set)
+CV_init, HI_init, CV_curr, HI_curr, cr_pval = PD_tests().Herfindahl(development_set[['grade', 'Default_Binary']], validation_set[['grade', 'Default_Binary']], 'grade', 'Default_Binary', 'grade', 'count')
+CV_init_exp, HI_init_exp, CV_curr_exp, HI_curr_exp, cr_pval_exp = PD_tests().Herfindahl(development_set[['grade', 'Default_Binary', 'original_exposure']], validation_set[['grade', 'Default_Binary', 'original_exposure']], 'grade', 'Default_Binary', 'original_exposure', 'sum')
 
-
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"""""""""""""""""""""""""""""""""""" "Loss Given Default" """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-### Predictive Ability (2.6.2)
-### LGD back-testing using a t-test (2.6.2.1)
+#####LGD
+####### Predictive Ability (2.6.2)
+####### LGD back-testing using a t-test (2.6.2.1)
 LGD_backtesting_ptf = LGD_tests().backtesting(development_set, "LGD", "LGD_realised")
 LGD_backtesting_perGrade = LGD_tests().backtesting_facilityGrade(development_set, "LGD", "LGD_realised")
-
-### Discriminatory Power (2.6.3)
-### gAUC for LGD (2.6.3.1)
+####### Discriminatory Power (2.6.3)
+####### gAUC for LGD (2.6.3.1)
 dev_LGD_transition_matrix        = matrix().matrix_obs(development_set, 'Bin_LGD', 'Bin_LGD_realised', 'Default_Binary')
 dev_LGD_transition_matrix_freq   = matrix().matrix_prob(dev_LGD_transition_matrix)
 val_LGD_transition_matrix        = matrix().matrix_obs(validation_set, 'Bin_LGD', 'Bin_LGD_realised', 'Default_Binary')
@@ -129,4 +121,6 @@ CCF_excel_inputs = {
     "stability_migration_matrix": [z_up, z_low, zUP_pval, zDOWN_pval],
 }
 export().CCF_toExcel(development_set, CCF_excel_inputs)
+
+
 
