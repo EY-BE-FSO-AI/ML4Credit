582e0614bb5f18b07d871dd0a945d911c86c9509
Bram Verbeke <bram.verbek@gmail.com>
2019-06-19 20:52:38 +0200

    Selected data in make_dataset.py

--- PD/src/data/make_dataset.py
+++ PD/src/data/make_dataset.py
@@ -1,48 +1,81 @@
 import pandas as pd
+import warnings # current version of seaborn generates a bunch of warnings that we'll ignore
+warnings.filterwarnings("ignore")
 
-def make_dataset():
-    print("make_dataset started")
-    #  The features of Acquisition file
-    col_acq = ['LoanID', 'Channel', 'SellerName', 'OrInterestRate', 'OrUnpaidPrinc', 'OrLoanTerm',
-               'OrDate', 'FirstPayment', 'OrLTV', 'OrCLTV', 'NumBorrow', 'DTIRat', 'CreditScore',
-               'FTHomeBuyer', 'LoanPurpose', 'PropertyType', 'NumUnits', 'OccStatus', 'PropertyState',
-               'Zip', 'MortInsPerc', 'ProductType', 'CoCreditScore', 'MortInsType', 'RelMortInd']
+if __name__ == "__main__":
+    print("yes")
 
-    #  The features of Performance file
-    col_per = ['LoanID', 'MonthRep', 'Servicer', 'CurrInterestRate', 'CAUPB', 'LoanAge', 'MonthsToMaturity',
-               'AdMonthsToMaturity', 'MaturityDate', 'MSA', 'CLDS', 'ModFlag', 'ZeroBalCode', 'ZeroBalDate',
-               'LastInstallDate', 'ForeclosureDate', 'DispositionDate', 'PPRC', 'AssetRecCost', 'MHRC',
-               'ATFHP', 'NetSaleProceeds', 'CreditEnhProceeds', 'RPMWP', 'OFP', 'NIBUPB', 'PFUPB', 'RMWPF',
-               'FPWA', 'ServicingIndicator']
+    """
+        Import datasets, select features and define the default-flag collumn.
+    """
+    
+    col_acq = ['LoanID','Channel','SellerName','OrInterestRate','OrUnpaidPrinc','OrLoanTerm',
+            'OrDate','FirstPayment','OrLTV','OrCLTV','NumBorrow','DTIRat','CreditScore',
+            'FTHomeBuyer','LoanPurpose','PropertyType','NumUnits','OccStatus','PropertyState',
+            'Zip','MortInsPerc','ProductType','CoCreditScore','MortInsType','RelMortInd']
+    
+    extended_selec_acq = ['LoanID', 'OrLTV', 'LoanPurpose', 'DTIRat', 'PropertyType', 'FTHomeBuyer', 'Channel', 'SellerName','OrInterestRate', 'CreditScore', 'NumBorrow', 'OrDate'] 
+    col_acq_subset = extended_selec_acq 
+    
+    col_per = ['LoanID','MonthRep','Servicer','CurrInterestRate','CAUPB','LoanAge','MonthsToMaturity',
+              'AdMonthsToMaturity','MaturityDate','MSA','CLDS','ModFlag','ZeroBalCode','ZeroBalDate',
+              'LastInstallDate','ForeclosureDate','DispositionDate','PPRC','AssetRecCost','MHRC',
+              'ATFHP','NetSaleProceeds','CreditEnhProceeds','RPMWP','OFP','NIBUPB','PFUPB','RMWPF',
+              'FPWA','ServicingIndicator'] 
+    
+    extended_selec_per = ['LoanID', 'MonthsToMaturity', 'CurrInterestRate', 'ForeclosureDate', 'LoanAge', 'CLDS', 'MaturityDate','ZeroBalCode', 'MonthRep']
+    
+    col_per_subset =  extended_selec_per 
+    
+    lines_to_read = 1000000
+    aquisition_frame = pd.read_csv('C:/Users/bebxadvberb/Documents/AI/Trusted AI/Acquisition_2007Q4.txt', sep='|', names=col_acq, usecols=col_acq_subset, index_col=False, nrows=lines_to_read )
+    performance_frame = pd.read_csv('C:/Users/bebxadvberb/Documents/AI/Trusted AI/Performance_2007Q4.txt', sep='|', names=col_per, usecols=col_per_subset, index_col=False, nrows=lines_to_read) 
 
-    extended_selec_acq = ['LoanID', 'OrLTV', 'LoanPurpose', 'DTIRat', 'PropertyType', 'FTHomeBuyer', 'Channel',
-                          'SellerName', 'OrInterestRate', 'CreditScore', 'NumBorrow']
-    extended_selec_per = ['LoanID', 'MonthsToMaturity', 'CurrInterestRate', 'ForeclosureDate', 'LoanAge']
-
-
-    linesToRead = None
-    linesToRead = 30000
-
-    aquisition_frame = pd.read_csv('C:/Users/bebxadvberb/Documents/AI/Trusted AI/Acquisition_2007Q4.txt',
-                                   sep='|', names=col_acq, nrows=linesToRead)
-    performance_frame = pd.read_csv('C:/Users/bebxadvberb/Documents/AI/Trusted AI/Performance_2007Q4.txt',
-                                    sep='|', names=col_per, index_col=False, nrows=linesToRead)
-
-    print("Acquisition Shape: ")
-    print(aquisition_frame.shape)
-    print("Performance Shape: ")
-    print(performance_frame.shape)
-
-
-    # Remove Duplicates (still in doubt if we have to do this)
-    # performance_frame.drop_duplicates(subset='LoanID', keep='last', inplace=True)
-    # print(performance_frame.shape)
-
-    # Merge the two DF's together using inner join
-    df = pd.merge(aquisition_frame, performance_frame, on='LoanID', how='inner')
-    # merged_frame.rename(index=str, columns={'ForeclosureDate': 'Default'}, inplace=True)
-
-
-    # export_csv = merged_frame.to_csv('C:/Users/bebxadvberb/PycharmProjects/ML4Credit/POC PD modelling/merged_df.csv')
-
-    return df
+    """ Fix the IDs in the observation set by fixing their reporting date AND requiring that the files are healthy. """
+    
+    observation_frame = performance_frame[(performance_frame.MonthRep == '12/01/2017') & 
+                                (   (performance_frame.CLDS == '0') | 
+                                    (performance_frame.CLDS == '1') | 
+                                    (performance_frame.CLDS == '2')
+                                )
+                                ]
+    obs_ids = observation_frame.LoanID
+    
+    """ Load only the observation IDs in the performance frame initially. """
+    pf = performance_frame[performance_frame.LoanID.isin(obs_ids)]
+    
+    """ Keep only the reporting dates that are in our performance period (MM/DD/YYYY format). """
+    pf_obs = pf[
+                    (pf.MonthRep == '01/01/2018') | 
+                    (pf.MonthRep == '02/01/2018') |
+                    (pf.MonthRep == '03/01/2018') |
+                    (pf.MonthRep == '04/01/2018') |
+                    (pf.MonthRep == '05/01/2018') |
+                    (pf.MonthRep == '06/01/2018') |
+                    (pf.MonthRep == '07/01/2018') |
+                    (pf.MonthRep == '08/01/2018') |
+                    (pf.MonthRep == '09/01/2018') |
+                    (pf.MonthRep == '10/01/2018') |
+                    (pf.MonthRep == '11/01/2018') |
+                    (pf.MonthRep == '12/01/2018') 
+                ]
+    
+    """ 
+    Find the LoanIDs of those loans where a default appears in our performance period.
+    """
+    pf_obs_defaults = pf_obs[
+                            (pf_obs.CLDS != '0') &
+                            (pf_obs.CLDS != '1') &
+                            (pf_obs.CLDS != '2') &
+                            (pf_obs.CLDS != 'X')
+                        ].LoanID
+    
+    pf_obs_defaults.drop_duplicates(keep='last', inplace=True)
+    
+    """ Merge the acquisition and performance frames. """
+    df = pd.merge(aquisition_frame, observation_frame, on = 'LoanID', how='inner')
+    
+    df['Default'] = 0
+    df.loc[df['LoanID'].isin(pf_obs_defaults), 'Default'] = 1
+    
+    df.to_csv('C:/Users/bebxadvberb/Documents/AI/Trusted AI/ML4Credit/PD/df.csv')
