3f81c960a21f6715d2dffc3645b674946d1824c9
Brent Oeyen <brent.oeyen@be.ey.com>
2019-09-15 13:49:29 +0200

    Added validation test for discriminatory power (AUC according to WMU)

--- PD/src/models/Validation_tests.py
+++ PD/src/models/Validation_tests.py
@@ -22,15 +22,15 @@ df_CL['training']        = 1
 df_dummy_CL              = pd.read_csv(os.getcwd()+'\predictions_validation_cl.csv')[['LoanID','Default','Prediction']]
 df_dummy_CL['training']  = 0
 df_CL                    = df_CL.append(df_dummy_CL).sort_values(by='LoanID').reset_index().iloc[:, 1:]
-df_CL = df_CL.rename(columns={"Default": "default", "Prediction": "pb_default"})
-df_CL = df_CL[['LoanID','pb_default','training','default']] #Re-arrange to match ML df.
+df_CL                    = df_CL.rename(columns={"Default": "default", "Prediction": "pb_default"})
+df_CL                    = df_CL[['LoanID','pb_default','training','default']] #Re-arrange to match ML df.
 
 # Add default flag to ML df
-df_ML = pd.merge(df_ML,df_CL[['LoanID','default']], on='LoanID', how='inner')
+df_ML                    = pd.merge(df_ML,df_CL[['LoanID','default']], on='LoanID', how='inner')
 
 # Master Scale
 ratings            = 22                                                                           #Number of rating grades
-PD_min             = 0.0001                                                                       #Minimum PD value (regulatory threshold)
+PD_min             = 0.0003                                                                       #Minimum PD value (regulatory threshold)
 slope              = (np.log((2-PD_min)/PD_min)-0)/(ratings-1)                                    #Slope between min PD value and default in logit space, depending on NB of rating grades
 MS                 = 2/(1+np.exp(slope*pd.Series(list(range(ratings)))))
 idx                = pd.IntervalIndex.from_arrays(MS[1:].append(pd.Series(0)), MS, closed='left')
@@ -38,7 +38,7 @@ df_ML['rating_PD'] = MS[idx.get_indexer(df_ML.pb_default)].values
 df_CL['rating_PD'] = MS[idx.get_indexer(df_CL.pb_default)].values
 
 # Validation test - Accuracy
-class PD_tests(object):      
+class CL_PD_tests(object):      
      ###Jeffrey's Test
      def Jeffrey(self, df, x, y , z):
           #df is a dataframe object that contains columns x, y and z
@@ -47,18 +47,20 @@ class PD_tests(object):
           #z observed variable (expected to be binary)
           #alpha = D + 1/2
           #beta = Nc- D + 1/2
-          aggregation                              = df.groupby(x).agg({x:'count', y: ['sum', 'count', 'mean'], z: ['sum', 'count', 'mean']})
-          aggregation['Observed']                  = aggregation[(z, 'mean')]
-          aggregation['alpha']                     = aggregation[(z, 'sum')] + 1/2
-          aggregation['beta']                      = aggregation[(z, 'count')] - aggregation['alpha'] + 1
-          aggregation['H0']                        = aggregation[(y, 'mean')]
-          aggregation['p_val']                     = beta.cdf(aggregation['H0'], aggregation['alpha'], aggregation['beta'])
-          aggregation.loc['Portfolio']             = aggregation.sum()
-          aggregation['Observed'].loc['Portfolio'] = df.agg({z: 'mean'}).values
-          aggregation['alpha'].loc['Portfolio']    = df.agg({z: 'sum' }).values + 1/2
-          aggregation['beta'].loc['Portfolio']     = df.agg({z: 'count'}).values - aggregation['alpha'].loc['Portfolio'] + 1
-          aggregation['H0'].loc['Portfolio']       = df.agg({y: 'mean'}).values
-          aggregation['p_val'].loc['Portfolio']    = beta.cdf(aggregation['H0'].loc['Portfolio'], aggregation['alpha'].loc['Portfolio'], aggregation['beta'].loc['Portfolio'])
+          aggregation                                 = df.groupby(x).agg({x:'count', y: ['sum', 'count', 'mean'], z: ['sum', 'count', 'mean']})
+          aggregation['Observed']                     = aggregation[(z, 'mean')]
+          aggregation['alpha']                        = aggregation[(z, 'sum')] + 1/2
+          aggregation['beta']                         = aggregation[(z, 'count')] - aggregation['alpha'] + 1
+          aggregation['H0']                           = aggregation[(y, 'mean')]
+          aggregation['p_val']                        = beta.cdf(aggregation['H0'], aggregation['alpha'], aggregation['beta'])
+          aggregation.loc['Portfolio']                = aggregation.sum()
+          aggregation[y, 'mean'].loc['Portfolio']     = df.agg({y: 'mean'}).values
+          aggregation[z, 'mean'].loc['Portfolio']     = df.agg({z: 'mean'}).values
+          aggregation['Observed'].loc['Portfolio']    = df.agg({z: 'mean'}).values
+          aggregation['alpha'].loc['Portfolio']       = df.agg({z: 'sum' }).values + 1/2
+          aggregation['beta'].loc['Portfolio']        = df.agg({z: 'count'}).values - aggregation['alpha'].loc['Portfolio'] + 1
+          aggregation['H0'].loc['Portfolio']          = df.agg({y: 'mean'}).values
+          aggregation['p_val'].loc['Portfolio']       = beta.cdf(aggregation['H0'].loc['Portfolio'], aggregation['alpha'].loc['Portfolio'], aggregation['beta'].loc['Portfolio'])
           return aggregation
           #return:
           #for portfolio and rating classes:
@@ -67,21 +69,52 @@ class PD_tests(object):
           #3. The number of observations (=N)
           #4. The number of flag=1 (sum of Defaults in case of PD)
           #5. The p-value (one-sided, y > D/N)
+     ### Plot ROC
      def ROC_curve(self, df, prediction, default):
           df_train = df[df.training == 1]
           df_valid = df[df.training == 0]
           fpr_train, tpr_train, thresholds = roc_curve(list(df_train[default].values), list(df_train[prediction].values))
           fpr_valid, tpr_valid, thresholds = roc_curve(list(df_valid[default].values), list(df_valid[prediction].values))
           return fpr_train, tpr_train, fpr_valid, tpr_valid
+     ###Discrimantory power test: Area Under the Curve###
+     def AUC(self, x, y):
+          #x : Observed parameter
+          #y : Modelled parameter
+          #z : Indicate whether VaR calculation is required [True or False]
+          ###Calculate parameters###
+          N1         = x.sum()
+          N2         = len(x)-N1
+          R          = y.rank()
+          R1         = R.loc[x == True]
+          R2         = R.loc[x == False]
+          ###Calculate Area Under the Curve###
+          U   = N1 * N2 + N1 * (N1 + 1)/2 - R1.sum()
+          AUC = U / (N1 * N2)
+          #AUC = u.ab.sum()/ (N1 * N2) #Not used (alternative calculation)
+          return AUC
 
-acc_ML = PD_tests().Jeffrey(df_ML, 'rating_PD', 'pb_default', 'default')
-acc_CL = PD_tests().Jeffrey(df_CL, 'rating_PD', 'pb_default', 'default')
+# Output validation tests
+## Accuracy
+acc_ML = CL_PD_tests().Jeffrey(df_ML, 'rating_PD', 'pb_default', 'default')
+acc_CL = CL_PD_tests().Jeffrey(df_CL, 'rating_PD', 'pb_default', 'default')
 print(acc_ML)
 print(acc_CL)
-
-fpr_ML_train, tpr_ML_train, fpr_ML_valid, tpr_ML_valid = PD_tests().ROC_curve(df_ML, 'pb_default', 'default')
-fpr_CL_train, tpr_CL_train, fpr_CL_valid, tpr_CL_valid = PD_tests().ROC_curve(df_CL, 'pb_default', 'default')
-
+## Distriminatory power
+fpr_ML_train, tpr_ML_train, fpr_ML_valid, tpr_ML_valid = CL_PD_tests().ROC_curve(df_ML, 'pb_default', 'default')
+fpr_CL_train, tpr_CL_train, fpr_CL_valid, tpr_CL_valid = CL_PD_tests().ROC_curve(df_CL, 'pb_default', 'default')
+AUC = np.zeros(6).reshape(3,2)
+j   = 0
+for i in ['CL', 'ML']:
+  AUC[0, j] = CL_PD_tests().AUC(vars()['df_'+i].default,                              vars()['df_'+i].rating_PD)
+  AUC[1, j] = CL_PD_tests().AUC(vars()['df_'+i].default[vars()['df_'+i].training==1], vars()['df_'+i].rating_PD[vars()['df_'+i].training==1])
+  AUC[2, j] = CL_PD_tests().AUC(vars()['df_'+i].default[vars()['df_'+i].training==0], vars()['df_'+i].rating_PD[vars()['df_'+i].training==0])
+  j     += 1
+print(AUC)
+## Excel
+with pd.ExcelWriter(os.getcwd()+'\Validation_output.xlsx') as writer:
+  acc_ML.to_excel(writer, sheet_name='Jeffrey ML')
+  acc_CL.to_excel(writer, sheet_name='Jeffrey CL')
+  pd.DataFrame(AUC).to_excel(writer , sheet_name='AUC')
 """
     Set the plotting stats.
 """
@@ -94,7 +127,7 @@ sns.set_style("darkgrid", {'font.family': ['EYInterstate']})
 """
 
 plt.figure(1)
-diag_line = np.linspace(0, 1, len(df_train_cl))
+diag_line = np.linspace(0, 1, len(df_CL.default))
 plt.plot(diag_line, diag_line, linestyle='--', c='darkgrey')
 plt.plot(fpr_ML_valid, tpr_ML_valid, label = "ML (Validation)" + ' : AUC = %0.4f' % auc(fpr_ML_valid,tpr_ML_valid), color='darkblue')
 plt.plot(fpr_ML_train, tpr_ML_train, label = "ML (Training)" + ' : AUC = %0.4f' % auc(fpr_ML_train, tpr_ML_train), color='darkblue', ls='--')
@@ -104,4 +137,4 @@ plt.xlabel('True positive rate')
 plt.ylabel('False positive rate')
 plt.title('Lorenz curve')
 plt.legend(loc=4)
-
+plt.show()
